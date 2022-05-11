48414c96b25702e2a4f9c7b9911e85b3d9b63e2e
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-05-11 12:50:39 +0530

    react-table

---
+++ src/component/SortingTable.js
@@ -0,0 +1,66 @@
+import React, { useMemo } from "react";
+import { useTable, useSortBy } from "react-table";
+import mock from "./mock.json";
+import { Columns } from "./columns";
+import "./Table.css";
+
+const SortingTable = () => {
+  const columns = useMemo(() => Columns, []);
+  const data = useMemo(() => mock, []);
+  const {
+    getTableBodyProps,
+    getTableProps,
+    headerGroups,
+    rows,
+    footerGroups,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy
+  );
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                {column.render("Header")}
+                <span>
+                  {column.isSorted ? (column.isSortedDesc ? "-" : "+") : ""}
+                </span>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+      <tfoot>
+        {footerGroups.map((footerGroup) => (
+          <tr {...footerGroup.getFooterGroupProps()}>
+            {footerGroup.headers.map((column) => (
+              <td {...column.getFooterProps}>{column.render("Footer")}</td>
+            ))}
+          </tr>
+        ))}
+      </tfoot>
+    </table>
+  );
+};
+
+export default SortingTable;
---
+++ src/component/columns.js
@@ -0,0 +1,79 @@
+export const Columns = [
+  {
+    Header: "id",
+    Footer: "id",
+    accessor: "id",
+  },
+  {
+    Header: "First Name",
+    Footer: "First Name",
+    accessor: "first_name",
+  },
+  {
+    Header: "Last Name",
+    Footer: "Last Name",
+    accessor: "last_name",
+  },
+  {
+    Header: "Email",
+    Footer: "Email",
+    accessor: "email",
+  },
+  {
+    Header: "Date of Birth",
+    Footer: "Date of Birth",
+    accessor: "date_of_birth",
+  },
+  {
+    Header: "Age",
+    Footer: "Age",
+    accessor: "age",
+  },
+];
+
+//Groups Header & Columns
+
+export const GROUPED_COLUMNS = [
+  {
+    Header: "Id",
+    Footer: "Id",
+    accessor: "id",
+  },
+  {
+    Header: "Name",
+    Footer: "Name",
+    columns: [
+      {
+        Header: "First Name",
+        Footer: "First Name",
+        accessor: "first_name",
+      },
+      {
+        Header: "Last Name",
+        Footer: "Last Name",
+        accessor: "last_name",
+      },
+    ],
+  },
+  {
+    Header: "Info",
+    Footer: "Info",
+    columns: [
+      {
+        Header: "Email",
+        Footer: "Email",
+        accessor: "email",
+      },
+      {
+        Header: "Date of Birth",
+        Footer: "Date of Birth",
+        accessor: "date_of_birth",
+      },
+      {
+        Header: "Age",
+        Footer: "Age",
+        accessor: "age",
+      },
+    ],
+  },
+];
